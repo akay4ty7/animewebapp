0dad3c71d699baf2c7675ac26c89cd67678d7902
19Maaka <62680289+19Maaka@users.noreply.github.com>
2021-02-25 15:05:59 +0800

    add test upload

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, url_for, request, redirect
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the web application."
+    return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run()
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+
+def allow_image(filename):
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+@app.route("/handleUpload", methods=['post'])
+def handleFileUpload():
+    if 'photo' in request.files:
+        photo = request.files['photo']
+        if photo.filename != ' ' :
+            if allow_image(photo.filename):
+                filename = secure_filename(photo.filename)
+                photo.save(os.path.join('D:/new main folider/testSerUpload', filename))
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
