f376202c787bb27e1c8b3d33a1cebf7e446a64ab
1538586299 <70641950+1538586299@users.noreply.github.com>
2021-03-01 15:00:40 +1300

    Update and rename test2.py to imageProcessing(grayscale).py

--- ImageProcessing/imageProcessing(grayscale).py
+++ ImageProcessing/imageProcessing(grayscale).py
@@ -12,10 +12,10 @@ def resize(imgPath,savePath):
  for file in files:
      fileType = os.path.splitext(file)
      if fileType[1] == '.png':
-        new_png = Image.open(imgPath+'/'+file) #打开图片
-        #new_png = new_png.resize((1000, 1000),Image.ANTIALIAS) #改变图片大小
+        new_png = Image.open(imgPath+'/'+file) #openImage
+        #new_png = new_png.resize((1000, 1000),Image.ANTIALIAS) #ResizeImage
         new_png = new_png.convert('L') # convert image to black and white
-        new_png.save(savePath+'/'+file) #保存图片
+        new_png.save(savePath+'/'+file) #saveImage
         print('down!')
         print('****************')
 
