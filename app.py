a9eaea46ec43b31d80d6e48379e44066a3e784b5
19Maaka <62680289+19Maaka@users.noreply.github.com>
2021-03-01 16:44:25 +0800

    upload, download function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory, abort, send_file, safe_join
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -7,10 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html')
-
+#only allow certain image style to be upload
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
 
 def allow_image(filename):
+    #check if the file upload include "." eg: abc.PNG
     if not "." in filename:
         return False
 
@@ -21,6 +22,13 @@ def allow_image(filename):
     else:
         return False
 
+app.config["CLIENT_IMAGES"] = "D:/new main folider/Github clones/animewebapp/static/client/img"
+
+@app.route("/get-image", methods=['post'])
+def get_image():
+    return send_from_directory(app.config["CLIENT_IMAGES"], filename='a.png', as_attachment=True)
+
+
 @app.route("/handleUpload", methods=['post'])
 def handleFileUpload():
     if 'photo' in request.files:
@@ -28,8 +36,10 @@ def handleFileUpload():
         if photo.filename != ' ' :
             if allow_image(photo.filename):
                 filename = secure_filename(photo.filename)
-                photo.save(os.path.join('D:/new main folider/testSerUpload', filename))
+                #the place to store image
+                photo.save(os.path.join('D:/new main folider/Github clones/animewebapp/static/client/img', filename))
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
